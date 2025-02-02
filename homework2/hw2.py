e4d0b607c725c603cc2e06d450ccf22a36ceecd8
bpeck114 <124009822+bpeck114@users.noreply.github.com>
2025-02-02 08:20:53 -0800

    Create hw2.py

---
+++ homework2/hw2.py
@@ -0,0 +1,7 @@
+# 1) Suppose your current working directory is ~/Desktop/classes/python_decal/. What command would allow you to navigate directly to ~/Desktop?
+# 2) What does ~/ mean?
+# 3) What's the difference between and abolute and a relative path in your terminal?
+# 4) What command would bring you back to your homework directory?
+# 5) What has been the most frustrating error or bug you've encountered in this class so far? How did you troubleshoot and resolve it?
+# 6) How does cloning a repository differ from pulling from a repository?
+# 7) Tell me a fun fact!
