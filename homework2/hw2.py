f6f22a9386c65666be31f28936c880dd25200a85
bpeck114 <124009822+bpeck114@users.noreply.github.com>
2025-02-04 14:43:18 -0800

    Update hw2.py

--- homework2/hw2.py
+++ homework2/hw2.py
@@ -4,6 +4,7 @@
 # 4) What command would bring you back to your home directory?
 # 5) If you called rm ./ in your current working directory, what would it do? 
 # 6) In your current working directory, what does calling "git add" do? What about "git commit"? What about "git push"?
-# 7) What has been the most frustrating error or bug you've encountered in this class so far? How did you troubleshoot and resolve it?
-# 8) How does cloning a repository differ from pulling from a repository?
-# 9) Tell me a fun fact!
+# 7) Let's say you call "git add" in your current working directory and then "git status". What message would appear? What is it telling you?
+# 8) What has been the most frustrating error or bug you've encountered in this class so far? How did you troubleshoot and resolve it?
+# 9) How does cloning a repository differ from pulling from a repository?
+# 10) Tell me a fun fact!
